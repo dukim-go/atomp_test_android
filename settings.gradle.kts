59dfc1e1e961ca9b10764f23a3338f0748d67d86
김대웅 <dukim.dev@lgcns.com>
2024-01-05 11:51:38 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "atomp_test_android"
+include(":app")
